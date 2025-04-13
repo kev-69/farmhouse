e4a287c9beef18cb33695a20d536e0466c0b09b4
kev-69 <bismarkobuobi19@gmail.com>
2025-04-13 11:22:06 +0000

    Change database dialect from MySQL to PostgreSQL in db-config.js

--- server/services/auth-services/src/config/db-config.js
+++ server/services/auth-services/src/config/db-config.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mysql',
+        dialect: 'postgres',
         port: process.env.DB_PORT,
         logging: false,
     }
