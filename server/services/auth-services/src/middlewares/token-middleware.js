4aa1e1c33c08933f0ab44e6a213287a202cb2257
kev-69 <bismarkobuobi19@gmail.com>
2025-04-15 11:59:30 +0000

    Refactor token validation middleware to use Authorization header for token retrieval

--- server/services/auth-services/src/middlewares/token-middleware.js
+++ server/services/auth-services/src/middlewares/token-middleware.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+const { JWT_SECRET, JWT_EXPIRATION } = process.env;
 
 const validateToken = (req, res, next) => {
-    const { token } = req.body;
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-        return res.status(400).json({ error: 'Token is required.' });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(400).json({ error: 'Authorization header with Bearer token is required.' });
     }
 
-    const { JWT_SECRET, JWT_EXPIRATION } = process.env;
+    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET, { maxAge: JWT_EXPIRATION });
