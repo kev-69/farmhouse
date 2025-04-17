b68931a4875548d9b53722b24281fbaf6b6ec5f8
kev-69 <bismarkobuobi19@gmail.com>
2025-04-17 07:30:40 +0000

    Add database configuration file for Sequelize connection

---
+++ server/shared/config/db-config.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false,
+    }
+)
+
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize;
