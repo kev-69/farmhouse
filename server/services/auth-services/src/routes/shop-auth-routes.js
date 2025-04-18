e27b3284f41604dc1bb86a180769aadabe0ac121
kev-69 <bismarkobuobi19@gmail.com>
2025-04-18 21:46:00 +0000

    Add shop authentication routes for shop registration and login

---
+++ server/services/auth-services/src/routes/shop-auth-routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    createShop,
+    loginShop,
+    verifyShopEmail,
+    resetShopPassword,
+    requestPasswordReset
+} = require('../controllers/shop-auth-controller');
+
+const validateLoginCredentials = require('../middlewares/shop-login-middleware');
+const validatePassword = require('../middlewares/password-middleware');
+const validateEmail = require('../middlewares/email-middleware');
+const validateToken = require('../middlewares/token-middleware');
+const authorizeRole = require('../middlewares/authorize-role');
+
+// routes
+router.post('/register-shop', createShop);
+router.post('/login-shop', validateLoginCredentials, loginShop);
+
+router.get('/verify-shop-email', validateToken, verifyShopEmail);
+
+router.post('/request-password-reset', validateEmail, requestPasswordReset);
+router.post('/reset-shop-password', validateToken, validatePassword, resetShopPassword);
+
+module.exports = router;
