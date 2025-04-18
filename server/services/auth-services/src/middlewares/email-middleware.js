823a9d00136e834c2687bd316d749d8552668e53
kev-69 <bismarkobuobi19@gmail.com>
2025-04-18 21:45:38 +0000

    Update email validation regex in email middleware

--- server/services/auth-services/src/middlewares/email-middleware.js
+++ server/services/auth-services/src/middlewares/email-middleware.js
@@ -1,7 +1,7 @@
 const validateEmail = (req, res, next) => {
     const { email } = req.body;
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[_%+-]+@[a-zA-Z0–9. -]+\. [a-zA-Z]{2,}$/;
     if (!email || !emailRegex.test(email)) {
         return res.status(400).json({ error: 'A valid email address is required.' });
     }
