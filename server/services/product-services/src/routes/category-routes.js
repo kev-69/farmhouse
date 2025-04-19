c9cf6f1969da523b81ea691b8a72258df8193bf4
kev-69 <bismarkobuobi19@gmail.com>
2025-04-19 18:49:51 +0000

    Refactor category routes to include token validation and ownership verification middleware

--- server/services/product-services/src/routes/category-routes.js
+++ server/services/product-services/src/routes/category-routes.js
@@ -3,18 +3,22 @@ const router = express.Router()
 
 const {
     addCategory,
-    updateCategoryById,
+    updateCategory,
     getAllCategories,
     removeCategoryById
 } = require('../controllers/category-controller')
 
 // middlewares
-
+const {
+    validateCategoryCreation,
+    verifyCategoryOwnership
+} = require('../middlewares/category-middlewares')
+const validateToken = require('../middlewares/token-middleware')
 
 // routes
 router.get('/all', getAllCategories)
-router.post('/add-category', addCategory) //admin and store owners add categories
-router.put('/update-category/:id', updateCategoryById) // admin and store owners update categories
-router.delete('/delete-category/:id', removeCategoryById) // admins can update categories
+router.post('/add-category', validateToken, validateCategoryCreation, addCategory) //admin and store owners add categories
+router.put('/update-category/:id', validateToken, verifyCategoryOwnership, updateCategory) // admin and store owners update categories
+router.delete('/delete-category/:id', validateToken, verifyCategoryOwnership, removeCategoryById) // admins can update categories
 
 module.exports = router
