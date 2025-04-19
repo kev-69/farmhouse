22d5666ddc7105f2b9c57f4622aa02110c0195e8
kev-69 <bismarkobuobi19@gmail.com>
2025-04-19 10:39:11 +0000

    Refactor shop-auth routes to remove unused middleware and fix request-password-reset route

--- server/services/auth-services/src/routes/shop-auth-routes.js
+++ server/services/auth-services/src/routes/shop-auth-routes.js
@@ -11,9 +11,9 @@ const {
 
 const validateLoginCredentials = require('../middlewares/shop-login-middleware');
 const validatePassword = require('../middlewares/password-middleware');
-const validateEmail = require('../middlewares/email-middleware');
+// const validateEmail = require('../middlewares/email-middleware');
 const validateToken = require('../middlewares/token-middleware');
-const authorizeRole = require('../middlewares/authorize-role');
+// const authorizeRole = require('../middlewares/authorize-role');
 
 // routes
 router.post('/register-shop', createShop);
@@ -21,7 +21,7 @@ router.post('/login-shop', validateLoginCredentials, loginShop);
 
 router.get('/verify-shop-email', validateToken, verifyShopEmail);
 
-router.post('/request-password-reset', validateEmail, requestPasswordReset);
+router.post('/request-password-reset', validateToken, requestPasswordReset);
 router.post('/reset-shop-password', validateToken, validatePassword, resetShopPassword);
 
 module.exports = router;
