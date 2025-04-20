11cdb70fa22edcb2a16d683ffeb13a38ae37ea86
kev-69 <bismarkobuobi19@gmail.com>
2025-04-20 10:06:41 +0000

    Refactor upload middleware to handle file uploads and improve error response

--- server/services/product-services/src/middlewares/upload.js
+++ server/services/product-services/src/middlewares/upload.js
@@ -1,15 +1,26 @@
-const multer = require('multer')
-const { CloudinaryStorage } = require('multer-storage-cloudinary')
-const cloudinary = require('../utils/cloudinary')
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../utils/cloudinary');
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'farmhouse_products',
         allowed_formats: ['jpg', 'png', 'jpeg'],
-    }
+    },
 });
 
 const upload = multer({ storage }).array('product_images', 10);
 
-module.exports = upload
+const uploadMiddleware = (req, res, next) => {
+    upload(req, res, function (err) {
+        if (err) {
+            // console.error('Error in upload middleware:', err); // Log the error
+            return res.status(500).json({ message: 'Error uploading files', error: err.message });
+        }
+        // console.log('Uploaded Files:', req.files); // Debug log
+        next();
+    });
+};
+
+module.exports = uploadMiddleware;
