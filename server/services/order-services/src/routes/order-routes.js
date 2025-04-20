a27c647d177a9e9a203bfaa57f15d5e21e787d30
kev-69 <bismarkobuobi19@gmail.com>
2025-04-20 17:37:56 +0000

    Add order routes for managing order history and checkout process

---
+++ server/services/order-services/src/routes/order-routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// controllers
+const {
+    createOrder,
+    updateOrder,
+    getOrderById,
+    getAllOrders,
+    deleteOrder,
+} = require('../controllers/order-controller');
+
+// middlewares
+const authenticateUser = require('../middlewares/cart-middlewares')
+const {
+    verifyUser,
+    validateOrderStatus,
+    authorizeShop,
+    validateCart
+} = require('../middlewares/order-middlewares')
+
+// routes
+router.get('/history',authenticateUser, getAllOrders)
+router.get('/history/:orderId',authenticateUser, getOrderById)
+router.post('/checkout',authenticateUser, verifyUser, validateCart, createOrder)
+router.put('/update-status/:orderId',authenticateUser, authorizeShop, validateOrderStatus, updateOrder)
+router.delete('/remove-order/:orderId',authenticateUser, authorizeShop, deleteOrder)
+
+module.exports = router
