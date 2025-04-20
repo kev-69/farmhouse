79ec7f34fae6ef6a27e304db0dcf1f53a94462ba
kev-69 <bismarkobuobi19@gmail.com>
2025-04-20 11:16:33 +0000

    Add ordersdb-config.js for database connection setup using Sequelize

---
+++ server/services/order-services/src/config/ordersdb-config.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const sequelize = new Sequelize(
+    process.env.ORDERS_DATABASE,
+    process.env.ORDERS_DATABASE_USER,
+    process.env.ORDERS_DATABASE_PASSWORD,
+    {
+        host: process.env.ORDERS_DATABASE_HOST,
+        dialect: 'postgres',
+        port: process.env.ORDERS_DATABASE_PORT,
+        logging: false, // Disable logging; default: console.log
+    }
+)
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.')
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err)
+    })
+
+module.exports = sequelize
